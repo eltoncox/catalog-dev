3174bf92a85020d1bcc83e9b007e662fb6493f50
eltoncox <elton.unix@gmail.com>
2025-08-27 12:36:15 -0300

    Database seeding .

--- backend/src/main/java/com/dev/elton/dscatalog/service/CategoryService.java
+++ backend/src/main/java/com/dev/elton/dscatalog/service/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dev.elton.dscatalog.entities.Category;
 import com.dev.elton.dscatalog.repositories.CategoryRepository;
@@ -12,8 +13,9 @@ import com.dev.elton.dscatalog.repositories.CategoryRepository;
 public class CategoryService {
 	
 	@Autowired
-	private CategoryRepository repository;
+	private CategoryRepository repository;	
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
