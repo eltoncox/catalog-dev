8ead5d74e1ac622a7f26b56b49923050e950f007
eltoncox <elton.unix@gmail.com>
2025-08-27 14:43:12 -0300

    Find exception handling.

--- backend/src/main/java/com/dev/elton/dscatalog/service/CategoryService.java
+++ backend/src/main/java/com/dev/elton/dscatalog/service/CategoryService.java
@@ -1,6 +1,7 @@
 package com.dev.elton.dscatalog.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dev.elton.dscatalog.dto.CategoryDTO;
 import com.dev.elton.dscatalog.entities.Category;
 import com.dev.elton.dscatalog.repositories.CategoryRepository;
+import com.dev.elton.dscatalog.service.exception.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -21,6 +23,13 @@ public class CategoryService {
 	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();			
 	    return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());		
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found !"));
+		return new CategoryDTO(entity);
 	}
 
 }
---
+++ backend/src/main/java/com/dev/elton/dscatalog/service/exception/EntityNotFoundException.java
@@ -0,0 +1,13 @@
+package com.dev.elton.dscatalog.service.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public EntityNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	
+
+}
