60241361ba47a51e38a8ac00d9ebc5c183998b9e
eltoncox <elton.unix@gmail.com>
2025-08-28 13:08:17 -0300

    Update category.

--- backend/src/main/java/com/dev/elton/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/dev/elton/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,13 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.dev.elton.dscatalog.service.exception.EntityNotFoundException;
+import com.dev.elton.dscatalog.service.exception.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandarError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandarError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		StandarError err = new StandarError();
 		
 		err.setTimestamp(Instant.now());
--- backend/src/main/java/com/dev/elton/dscatalog/service/exception/ResourceNotFoundException.java
+++ backend/src/main/java/com/dev/elton/dscatalog/service/exception/ResourceNotFoundException.java
@@ -1,13 +1,11 @@
 package com.dev.elton.dscatalog.service.exception;
 
-public class EntityNotFoundException extends RuntimeException {
+public class ResourceNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 	
-	public EntityNotFoundException(String msg) {
+	public ResourceNotFoundException(String msg) {
 		super(msg);
 	}
-	
-	
 
 }
