3a7b2746acfa2dabd38f08d1bdcf41859b0d3b14
eltoncox <elton.unix@gmail.com>
2025-08-31 01:44:55 -0300

    User CRUD.

---
+++ backend/src/main/java/com/dev/elton/dscatalog/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.dev.elton.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.elton.dscatalog.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
---
+++ backend/src/main/java/com/dev/elton/dscatalog/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dev.elton.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.elton.dscatalog.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
